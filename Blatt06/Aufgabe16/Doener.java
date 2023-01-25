c59e6bc8f55fbfa25be218779c3f3eb8fe7daa48
Lars Heimann <Lars.Heimann@zeb.de>
2023-01-25 10:31:53 +0100

    changed object names from doenerzusatz

--- Blatt06/Aufgabe16/Doener.java
+++ Blatt06/Aufgabe16/Doener.java
@@ -8,19 +8,19 @@ public abstract class Doener {
         System.out.println("\nDer Preis vom Testdöner ohne alles beträgt: "+testDoener.berechnePreis());
 
         // neue sauce hinzufügen
-        Doenerzusatz mildeCocktail = new Sauce("Etwas", "Cocktailsauce", testDoener);
-        System.out.println("\nDer Preis von der milden Cocktailsauce beträgt: "+mildeCocktail.berechneZusatzkosten());
-        System.out.println("Der Preis vom aktuellen Döner beträgt: "+mildeCocktail.berechnePreis());
+        Doenerzusatz zusatz1 = new Sauce("Etwas", "Cocktailsauce", testDoener);
+        System.out.println("\nDer Preis von der milden Cocktailsauce beträgt: "+zusatz1.berechneZusatzkosten());
+        System.out.println("Der Preis vom aktuellen Döner beträgt: "+zusatz1.berechnePreis());
 
         // 3 falafeln hinzufügen
-        Doenerzusatz special = new Special(30,3,mildeCocktail);
-        System.out.println("\nDer Preis von den Falafelbällchen beträgt: "+special.berechneZusatzkosten());
-        System.out.println("Der Preis vom aktuellen Döner beträgt: "+special.berechnePreis());
+        Doenerzusatz zusatz2 = new Special(30,3,zusatz1);
+        System.out.println("\nDer Preis von den Falafelbällchen beträgt: "+zusatz2.berechneZusatzkosten());
+        System.out.println("Der Preis vom aktuellen Döner beträgt: "+zusatz2.berechnePreis());
 
         // Mais als Beilage
-        Doenerzusatz mais = new Beilage(50,"Mais", special);
-        System.out.println("\nDer Preis vom Mais beträgt: "+mais.berechneZusatzkosten());
-        System.out.println("Der Preis vom aktuellen Döner beträgt: "+mais.berechnePreis());
+        Doenerzusatz zusatz3 = new Beilage(50,"Mais", zusatz2);
+        System.out.println("\nDer Preis vom Mais beträgt: "+zusatz3.berechneZusatzkosten());
+        System.out.println("Der Preis vom aktuellen Döner beträgt: "+zusatz3.berechnePreis());
     }
 }
 
